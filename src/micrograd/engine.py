b16919a3265e5d6f4e528bada004a940ee4b64db
tarun gupta <rajeshguptamsk12@gmail.com>
2026-09-17 00:32:29 +0530

    /, pow, exp, - functionality addition

---
+++ src/micrograd/engine.py
@@ -0,0 +1,102 @@
+import math
+
+class Value:
+    
+    def __init__(self, value, _children = (), op ='', label = ''):
+        self.data = value
+        self._prev = _children
+        self._op = op
+        self.label = label
+        self.grad = 0
+        self._backward = lambda: None
+        
+    def __repr__(self):
+        return f"Value(data={self.data})"
+        
+    def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad+= 1.0*out.grad
+            other.grad+= 1.0*out.grad
+        out._backward = _backward
+        return out
+        
+    def __radd__(self,other):
+        return self + other
+        
+    def __neg__(self):
+        return self*-1
+       
+    def __sub__(self,other):
+        return self + (-other)
+        
+    def __rsub__(self,other):
+        return -self + other
+    
+    def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+        
+        def _backward():
+            self.grad+= other.data*out.grad
+            other.grad+= self.data*out.grad
+        out._backward = _backward
+        return out
+
+    def __rmul__(self,other):
+        return self*other
+
+    def __truediv__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        out = self *other**-1
+        return out
+        
+    def __rtruediv__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
+        return other * (self ** -1)
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)) ## supporting int/float for now
+        out = Value(self.data**other, (self,), f'**{other}')
+
+        def _backward():
+            self.grad += other*(self.data**(other-1))*out.grad
+            
+        out._backward = _backward
+        return out
+        
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self,), 'exp')
+
+        def _backward():
+            self.grad+= out.data*out.grad
+        out._backward = _backward
+        return out
+    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
+        out = Value(t, (self,), op ='tanh')
+        def _backward():
+            self.grad+= (1-t**2)*out.grad
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        
+        topo = []
+        visited = set()
+        def build_topo(v):
+          if v not in visited:
+            visited.add(v)
+            for child in v._prev:
+              build_topo(child)
+            topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
